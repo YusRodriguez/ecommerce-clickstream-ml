02634d8392649b13894edddf2626433c388233f0
Carina Coliluan <caribelen04@gmail.com>
2026-07-22 23:27:03 +0100

    Actualizar URL de la API para Render

--- SRC/app_front.py
+++ SRC/app_front.py
@@ -12,7 +12,7 @@ st.set_page_config(
     layout="wide"
 )
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://ecommerce-clickstream-ml.onrender.com"
 
 # ==========================================================
 # LISTAS COMPARTIDAS DEL FORMULARIO
